694241e2af8c5b7ba600ba4f5b1ee55427d7961a
Davidsev05 <produccionesludens@gmail.com>
2023-09-22 07:19:02 -0500

    transacion

---
+++ js/olimpica.js
@@ -0,0 +1,9 @@
+let boxCarousel =document.getElementById("boxCarousel");
+
+boxCarousel.addEventListener("mouseover", function () {
+    if (boxCarousel.classList.contains("box-carousel-1")) {
+        boxCarousel.classList.remove("box-carousel-1");
+        boxCarousel.classList.add("box-carousel-2");
+    }
+    else if (boxCarousel.classList)
+});
